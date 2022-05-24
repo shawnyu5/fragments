6a6107d0f02c7b3ffdcba504928ff094f6030c17
shawn yu <shawn.yu5@hotmail.com>
2022-05-24 15:37:45 -0400

    fix: await get request

--- tests/unit/get.test.ts
+++ tests/unit/get.test.ts
@@ -7,8 +7,8 @@ describe("GET /v1/fragments", () => {
    test("unauthenticated requests are denied", () =>
       request(app).get("/v1/fragments").expect(401));
    // If the wrong username/password pair are used (no such user), it should be forbidden
-   test("incorrect credentials are denied", () => {
-      request(app)
+   test("incorrect credentials are denied", async () => {
+      await request(app)
          .get("/v1/fragments")
          .auth("invalid@email.com", "incorrect_password")
          .expect(401);
@@ -19,6 +19,7 @@ describe("GET /v1/fragments", () => {
       const res = await request(app)
          .get("/v1/fragments")
          .auth("user1@email.com", "password1");
+
       expect(res.statusCode).toBe(200);
       expect(res.body.status).toBe("ok");
       expect(Array.isArray(res.body.fragments)).toBe(true);
