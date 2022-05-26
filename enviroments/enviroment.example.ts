04d7e54fba44db31ce59b46ee25b9925fb200f7d
shawn yu <shawn.yu5@hotmail.com>
2022-05-26 16:04:45 -0400

    feat: updated example env file to have empty defaults

--- enviroments/enviroment.example.ts
+++ enviroments/enviroment.example.ts
@@ -2,9 +2,10 @@ export const enviroment = {
    PORT: 8080,
    LOG_LEVEL: "info",
    // AWS Amazon Cognito User Pool ID (use your User Pool ID)
-   AWS_COGNITO_POOL_ID: "ID",
+   AWS_COGNITO_POOL_ID: "",
    // AWS Amazon Cognito Client App ID (use your Client App ID)
-   AWS_COGNITO_CLIENT_ID: "ID",
-   API_URL: "URL:",
-   HTPASSWD_FILE: "LOCATION",
+   AWS_COGNITO_CLIENT_ID: "",
+   API_URL: "",
+   // location os .htpasswd file
+   HTPASSWD_FILE: "",
 };
