e7517e106adb084505c0d0f3715a90eb01730aea
shawn yu <shawn.yu5@hotmail.com>
2022-05-26 12:20:39 -0400

    chore: added example env file

---
+++ enviroments/enviroment.example.ts
@@ -0,0 +1,10 @@
+export const enviroment = {
+   PORT: 8080,
+   LOG_LEVEL: "info",
+   // AWS Amazon Cognito User Pool ID (use your User Pool ID)
+   AWS_COGNITO_POOL_ID: "ID",
+   // AWS Amazon Cognito Client App ID (use your Client App ID)
+   AWS_COGNITO_CLIENT_ID: "ID",
+   API_URL: "URL:",
+   HTPASSWD_FILE: "LOCATION",
+};
