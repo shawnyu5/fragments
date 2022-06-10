d1e58d832116bed4ffe23ae73d0da4e8f1fa448e
shawn yu <shawn.yu5@hotmail.com>
2022-06-10 12:10:59 -0400

    fix: enviroment.example

--- enviroments/enviroment.example.ts
+++ enviroments/enviroment.example.ts
@@ -1,10 +1,10 @@
 export const enviroment = {
-   PORT: 8080,
    LOG_LEVEL: "info",
    // AWS Amazon Cognito User Pool ID (use your User Pool ID)
    AWS_COGNITO_POOL_ID: "",
    // AWS Amazon Cognito Client App ID (use your Client App ID)
    AWS_COGNITO_CLIENT_ID: "",
+   API_URL: "",
    // location os .htpasswd file
    HTPASSWD_FILE: "",
    PRODUCTION: true,
